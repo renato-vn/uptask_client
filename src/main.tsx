7a34125e4064813d18f23fc416cb3d7ed69715b6
Renato Vicencio N <renatovicencion@gmail.com>
2024-10-07 13:32:51 -0300

    :lipstick: feat: add header ui and add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Router from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold underline">Hola</h1>
+    <Router />
   </StrictMode>
 );
